6710770f6c3b6f9fca3cdbb56a99372295ad692f
melody <35177464+HexCodeFFF@users.noreply.github.com>
2022-09-03 20:07:43 -0500

    this actually works!

--- libvips/create/text.c
+++ libvips/create/text.c
@@ -78,7 +78,7 @@
 
 #include <vips/vips.h>
 #include <vips/internal.h>
-
+//#define HAVE_PANGOCAIRO
 #ifdef HAVE_PANGOCAIRO
 
 #include <cairo.h>
@@ -118,7 +118,7 @@ G_DEFINE_TYPE( VipsText, vips_text, VIPS_TYPE_CREATE );
 
 /* ... single-thread vips_text_fontfiles with this.
  */
-static GMutex *vips_text_lock = NULL; 
+static GMutex *vips_text_lock = NULL;
 
 /* All the fontfiles we've loaded. fontconfig lets you add a fontfile
  * repeatedly, and we obviously don't want that.
@@ -130,14 +130,14 @@ vips_text_dispose( GObject *gobject )
 {
 	VipsText *text = (VipsText *) gobject;
 
-	VIPS_UNREF( text->layout ); 
-	VIPS_UNREF( text->context ); 
+	VIPS_UNREF( text->layout );
+	VIPS_UNREF( text->context );
 
 	G_OBJECT_CLASS( vips_text_parent_class )->dispose( gobject );
 }
 
 static PangoLayout *
-text_layout_new( PangoContext *context, 
+text_layout_new( PangoContext *context,
 	const char *text, const char *font, int width, int spacing,
 	VipsAlign align, gboolean justify )
 {
@@ -188,16 +188,16 @@ vips_text_get_extents( VipsText *text, VipsRect *extents )
 	PangoRectangle ink_rect;
 	PangoRectangle logical_rect;
 
-	pango_cairo_font_map_set_resolution( 
+	pango_cairo_font_map_set_resolution(
 		PANGO_CAIRO_FONT_MAP( text->fontmap ), text->dpi );
 
 	VIPS_UNREF( text->layout );
-	if( !(text->layout = text_layout_new( text->context, 
-		text->text, text->font, 
-		text->width, text->spacing, text->align, text->justify )) ) 
+	if( !(text->layout = text_layout_new( text->context,
+		text->text, text->font,
+		text->width, text->spacing, text->align, text->justify )) )
 		return( -1 );
 
-	pango_layout_get_pixel_extents( text->layout, 
+	pango_layout_get_pixel_extents( text->layout,
 		&ink_rect, &logical_rect );
 	extents->left = ink_rect.x;
 	extents->top = ink_rect.y;
@@ -205,11 +205,11 @@ vips_text_get_extents( VipsText *text, VipsRect *extents )
 	extents->height = ink_rect.height;
 
 #ifdef DEBUG
-	printf( "vips_text_get_extents: dpi = %d\n", text->dpi ); 
+	printf( "vips_text_get_extents: dpi = %d\n", text->dpi );
 	printf( "    ink left = %d, top = %d, width = %d, height = %d\n",
 		ink_rect.x, ink_rect.y, ink_rect.width, ink_rect.height );
 	printf( "    logical left = %d, top = %d, width = %d, height = %d\n",
-		logical_rect.x, logical_rect.y, 
+		logical_rect.x, logical_rect.y,
 		logical_rect.width, logical_rect.height );
 #endif /*DEBUG*/
 
@@ -221,7 +221,7 @@ vips_text_get_extents( VipsText *text, VipsRect *extents )
 static int
 vips_text_rect_difference( VipsRect *target, VipsRect *extents )
 {
-	if( vips_rect_includesrect( target, extents ) ) 
+	if( vips_rect_includesrect( target, extents ) )
 		return( -1 );
 	else
 		return( 1 );
@@ -257,9 +257,9 @@ vips_text_autofit( VipsText *text )
 		target.left, target.top, target.width, target.height );
 #endif /*DEBUG*/
 
-	for(;;) { 
+	for(;;) {
 		if( vips_text_get_extents( text, &extents ) )
-			return( -1 ); 
+			return( -1 );
 		target.left = extents.left;
 		target.top = extents.top;
 		difference = vips_text_rect_difference( &target, &extents );
@@ -271,7 +271,7 @@ vips_text_autofit( VipsText *text )
 
 		/* Stop if we straddle the target.
 		 */
-		if( difference != previous_difference ) 
+		if( difference != previous_difference )
 			break;
 
 		previous_difference = difference;
@@ -305,15 +305,15 @@ vips_text_autofit( VipsText *text )
 	/* Refine lower and upper until they are almost touching.
 	 */
 	while( upper_dpi - lower_dpi > 1 &&
-		difference != 0 ) { 
+		difference != 0 ) {
 		text->dpi = (upper_dpi + lower_dpi) / 2;
 		if( vips_text_get_extents( text, &extents ) )
-			return( -1 ); 
+			return( -1 );
 		target.left = extents.left;
 		target.top = extents.top;
 		difference = vips_text_rect_difference( &target, &extents );
 
-		if( difference < 0 ) 
+		if( difference < 0 )
 			lower_dpi = text->dpi;
 		else
 			upper_dpi = text->dpi;
@@ -323,17 +323,17 @@ vips_text_autofit( VipsText *text )
 	 * and we can use upper. Otherwise we are straddling the target and we
 	 * must take lower.
 	 */
-	if( difference == 0 ) 
+	if( difference == 0 )
 		text->dpi = upper_dpi;
-	else 
+	else
 		text->dpi = lower_dpi;
-	g_object_set( text, "autofit_dpi", text->dpi, NULL ); 
+	g_object_set( text, "autofit_dpi", text->dpi, NULL );
 
 #ifdef DEBUG
 	printf( "vips_text_autofit: final dpi = %d\n", text->dpi );
 #endif /*DEBUG*/
 
-	return( 0 ); 
+	return( 0 );
 }
 
 static int
@@ -353,7 +353,7 @@ vips_text_build( VipsObject *object )
 		return( -1 );
 
 	if( !pango_parse_markup( text->text, -1, 0, NULL, NULL, NULL, NULL ) ) {
-		vips_error( class->nickname, 
+		vips_error( class->nickname,
 			"%s", _( "invalid markup in text" ) );
 		return( -1 );
 	}
@@ -362,32 +362,38 @@ vips_text_build( VipsObject *object )
 	text->context = pango_font_map_create_context(
 		PANGO_FONT_MAP( text->fontmap ) );
 
-	g_mutex_lock( vips_text_lock ); 
+	g_mutex_lock( vips_text_lock );
 
 	if( !vips_text_fontfiles )
-		vips_text_fontfiles = 
+		vips_text_fontfiles =
 			g_hash_table_new( g_str_hash, g_str_equal );
-
+//#define HAVE_FONTCONFIG
 #ifdef HAVE_FONTCONFIG
-	if( text->fontfile &&
-		!g_hash_table_lookup( vips_text_fontfiles, text->fontfile ) ) {
-		/* This can fail if you eg. add the same font from two
-		 * different files. Just warn.
-		 */
-		if( !FcConfigAppFontAddFile( NULL, 
-			(const FcChar8 *) text->fontfile ) ) 
-			g_warning( _( "unable to load fontfile \"%s\"" ), 
-				text->fontfile );
-		g_hash_table_insert( vips_text_fontfiles, 
-			text->fontfile,
-			g_strdup( text->fontfile ) );
+	if( text->fontfile) {
+        char * token = strtok(text->fontfile, "\n");
+        while( token != NULL ) {
+            if(!g_hash_table_lookup( vips_text_fontfiles, token ))
+                /* This can fail if you eg. add the same font from two
+                 * different files. Just warn.
+                 */
 
-		/* We need to inform that pango should invalidate its
-		 * fontconfig cache whenever any changes are made.
-		 */
-		if( PANGO_IS_FC_FONT_MAP( text->fontmap ) )
-			pango_fc_font_map_cache_clear(
-				PANGO_FC_FONT_MAP( text->fontmap ) );
+                if( !FcConfigAppFontAddFile( NULL,
+                                             (const FcChar8 *) token ) )
+                    g_warning( _( "unable to load fontfile \"%s\"" ),
+                               token );
+            g_hash_table_insert( vips_text_fontfiles,
+                                 token,
+                                 g_strdup( token ) );
+
+            /* We need to inform that pango should invalidate its
+             * fontconfig cache whenever any changes are made.
+             */
+            if( PANGO_IS_FC_FONT_MAP( text->fontmap ) )
+                pango_fc_font_map_cache_clear(
+                        PANGO_FC_FONT_MAP( text->fontmap ) );
+            token = strtok(NULL, " ");
+        }
+
 	}
 #else /*!HAVE_FONTCONFIG*/
 	if( text->fontfile )
@@ -397,7 +403,7 @@ vips_text_build( VipsObject *object )
 
 	g_mutex_unlock( vips_text_lock );
 
-	/* If our caller set height and not dpi, we adjust dpi until 
+	/* If our caller set height and not dpi, we adjust dpi until
 	 * we get a fit.
 	 */
 	if( vips_object_argument_isset( object, "height" ) &&
@@ -411,7 +417,7 @@ vips_text_build( VipsObject *object )
 	if( vips_text_get_extents( text, &extents ) )
 		return( -1 );
 
-	if( extents.width == 0 || 
+	if( extents.width == 0 ||
 		extents.height == 0 ) {
 		vips_error( class->nickname, "%s", _( "no text to render" ) );
 		return( -1 );
@@ -419,28 +425,28 @@ vips_text_build( VipsObject *object )
 
 	/* Cairo can't go over 32k pixels.
 	 */
-	if( extents.width >= 32768 || 
+	if( extents.width >= 32768 ||
 		extents.height >= 32768 ) {
-		vips_error( class->nickname, 
+		vips_error( class->nickname,
 			"%s", _( "text image too large" ) );
 		return( -1 );
 	}
 
 	image = t[0] = vips_image_new_memory();
 	vips_image_init_fields( image,
-		extents.width, extents.height, 4, 
-		VIPS_FORMAT_UCHAR, VIPS_CODING_NONE, 
+		extents.width, extents.height, 4,
+		VIPS_FORMAT_UCHAR, VIPS_CODING_NONE,
 		VIPS_INTERPRETATION_sRGB,
 		text->dpi / 25.4, text->dpi / 25.4 );
 	image->Xoffset = extents.left;
 	image->Yoffset = extents.top;
 
 	if( vips_image_pipelinev( image, VIPS_DEMAND_STYLE_ANY, NULL ) ||
-		vips_image_write_prepare( image ) ) 
+		vips_image_write_prepare( image ) )
 		return( -1 );
 
-	surface = cairo_image_surface_create_for_data( 
-		VIPS_IMAGE_ADDR( image, 0, 0 ), 
+	surface = cairo_image_surface_create_for_data(
+		VIPS_IMAGE_ADDR( image, 0, 0 ),
 		CAIRO_FORMAT_ARGB32,
 		image->Xsize, image->Ysize,
 		VIPS_IMAGE_SIZEOF_LINE( image ) );
@@ -456,20 +462,20 @@ vips_text_build( VipsObject *object )
 	if( text->rgba ) {
 		int y;
 
-		/* Cairo makes pre-multipled BRGA -- we must byteswap and 
+		/* Cairo makes pre-multipled BRGA -- we must byteswap and
 		 * unpremultiply.
 		 */
-		for( y = 0; y < image->Ysize; y++ ) 
-			vips__premultiplied_bgra2rgba( 
-				(guint32 *) 
+		for( y = 0; y < image->Ysize; y++ )
+			vips__premultiplied_bgra2rgba(
+				(guint32 *)
 					VIPS_IMAGE_ADDR( image, 0, y ),
-				image->Xsize ); 
+				image->Xsize );
 	}
 	else {
 		/* We just want the alpha channel.
 		 */
 		if( vips_extract_band( image, &t[1], 3, NULL ) ||
-			vips_copy( t[1], &t[2], 
+			vips_copy( t[1], &t[2],
 				"interpretation", VIPS_INTERPRETATION_MULTIBAND,
 				NULL ) )
 			return( -1 );
@@ -485,7 +491,7 @@ vips_text_build( VipsObject *object )
 static void *
 vips_text_make_lock( void *client )
 {
-	if( !vips_text_lock ) 
+	if( !vips_text_lock )
 		vips_text_lock = vips_g_mutex_new();
 
 	return( NULL );
@@ -509,78 +515,78 @@ vips_text_class_init( VipsTextClass *class )
 	vobject_class->description = _( "make a text image" );
 	vobject_class->build = vips_text_build;
 
-	VIPS_ARG_STRING( class, "text", 4, 
-		_( "Text" ), 
+	VIPS_ARG_STRING( class, "text", 4,
+		_( "Text" ),
 		_( "Text to render" ),
 		VIPS_ARGUMENT_REQUIRED_INPUT,
 		G_STRUCT_OFFSET( VipsText, text ),
-		NULL ); 
+		NULL );
 
-	VIPS_ARG_STRING( class, "font", 5, 
-		_( "Font" ), 
+	VIPS_ARG_STRING( class, "font", 5,
+		_( "Font" ),
 		_( "Font to render with" ),
 		VIPS_ARGUMENT_OPTIONAL_INPUT,
 		G_STRUCT_OFFSET( VipsText, font ),
-		NULL ); 
+		NULL );
 
-	VIPS_ARG_INT( class, "width", 6, 
-		_( "Width" ), 
+	VIPS_ARG_INT( class, "width", 6,
+		_( "Width" ),
 		_( "Maximum image width in pixels" ),
 		VIPS_ARGUMENT_OPTIONAL_INPUT,
 		G_STRUCT_OFFSET( VipsText, width ),
 		0, VIPS_MAX_COORD, 0 );
 
-	VIPS_ARG_INT( class, "height", 7, 
-		_( "Height" ), 
+	VIPS_ARG_INT( class, "height", 7,
+		_( "Height" ),
 		_( "Maximum image height in pixels" ),
 		VIPS_ARGUMENT_OPTIONAL_INPUT,
 		G_STRUCT_OFFSET( VipsText, height ),
 		0, VIPS_MAX_COORD, 0 );
 
-	VIPS_ARG_ENUM( class, "align", 8, 
-		_( "Align" ), 
+	VIPS_ARG_ENUM( class, "align", 8,
+		_( "Align" ),
 		_( "Align on the low, centre or high edge" ),
 		VIPS_ARGUMENT_OPTIONAL_INPUT,
 		G_STRUCT_OFFSET( VipsText, align ),
 		VIPS_TYPE_ALIGN, VIPS_ALIGN_LOW );
 
-	VIPS_ARG_BOOL( class, "justify", 9, 
-		_( "Justify" ), 
+	VIPS_ARG_BOOL( class, "justify", 9,
+		_( "Justify" ),
 		_( "Justify lines" ),
 		VIPS_ARGUMENT_OPTIONAL_INPUT,
 		G_STRUCT_OFFSET( VipsText, justify ),
 		FALSE );
 
-	VIPS_ARG_INT( class, "dpi", 9, 
-		_( "DPI" ), 
+	VIPS_ARG_INT( class, "dpi", 9,
+		_( "DPI" ),
 		_( "DPI to render at" ),
 		VIPS_ARGUMENT_OPTIONAL_INPUT,
 		G_STRUCT_OFFSET( VipsText, dpi ),
 		1, 1000000, 72 );
 
-	VIPS_ARG_INT( class, "autofit_dpi", 10, 
-		_( "Autofit DPI" ), 
+	VIPS_ARG_INT( class, "autofit_dpi", 10,
+		_( "Autofit DPI" ),
 		_( "DPI selected by autofit" ),
 		VIPS_ARGUMENT_OPTIONAL_OUTPUT,
 		G_STRUCT_OFFSET( VipsText, dpi ),
 		1, 1000000, 72 );
 
-	VIPS_ARG_INT( class, "spacing", 11, 
-		_( "Spacing" ), 
+	VIPS_ARG_INT( class, "spacing", 11,
+		_( "Spacing" ),
 		_( "Line spacing" ),
 		VIPS_ARGUMENT_OPTIONAL_INPUT,
 		G_STRUCT_OFFSET( VipsText, spacing ),
 		0, 1000000, 0 );
 
-	VIPS_ARG_STRING( class, "fontfile", 12, 
-		_( "Font file" ), 
+	VIPS_ARG_STRING( class, "fontfile", 12,
+		_( "Font file" ),
 		_( "Load this font file" ),
 		VIPS_ARGUMENT_OPTIONAL_INPUT,
 		G_STRUCT_OFFSET( VipsText, fontfile ),
-		NULL ); 
+		NULL );
 
-	VIPS_ARG_BOOL( class, "rgba", 9, 
-		_( "RGBA" ), 
+	VIPS_ARG_BOOL( class, "rgba", 9,
+		_( "RGBA" ),
 		_( "Enable RGBA output" ),
 		VIPS_ARGUMENT_OPTIONAL_INPUT,
 		G_STRUCT_OFFSET( VipsText, rgba ),
@@ -593,7 +599,7 @@ vips_text_init( VipsText *text )
 {
 	text->align = VIPS_ALIGN_LOW;
 	text->dpi = 72;
-	VIPS_SETSTR( text->font, "sans 12" ); 
+	VIPS_SETSTR( text->font, "sans 12" );
 }
 
 #endif /*HAVE_PANGOCAIRO*/
